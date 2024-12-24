8103fe95a34440af53754841eefe1dbfd156cd2b
artem <v.amuzinsky@yandex.ru>
2024-12-25 01:01:00 +0300

    t80 в процессе

--- src/components/T80.jsx
+++ src/components/T80.jsx
@@ -1,5 +1,57 @@
-export function T80(){
-    return(
-        <h2>asdad</h2>
-    )
+import "../css/T80.css";
+import { useEffect, useState } from "react";
+import engine from "../music/engine_sound.mp3";
+
+export function T80({ onReturn }) {
+    const [scoped, setScoped] = useState(false);
+    const [audioStarted, setAudioStarted] = useState(true);
+    const audio = new Audio(engine);
+
+    useEffect(() => {
+        if (audioStarted) {
+            audio.loop = true;
+            audio.volume = 1;
+            audio.play().catch((error) => {
+                console.error("Ошибка воспроизведения аудио:", error);
+            });
+        }
+        return () => {
+            if (audioStarted) {
+                audio.pause();
+                audio.currentTime = 0;
+            }
+        };
+    }, [audioStarted]);
+
+
+    function makeSope() {
+        setScoped(true);
+        const container = document.querySelector('.t80-container');
+        container.classList.add('new-background');
+    }
+
+    function exitScope() {
+        setScoped(false);
+        const container = document.querySelector('.t80-container');
+        container.classList.remove('new-background');
+    }
+
+    function handleReturnToMain() {
+        audio.pause();
+        audio.currentTime = 0;
+        onReturn();
+    }
+
+    return (
+        <div className="t80-container">
+
+
+            <button onClick={handleReturnToMain} id="exit-button">Выйти</button>
+            {scoped ? (
+                <button id="scoped" onClick={exitScope}>Выйти из приближения</button>
+            ) : (
+                <button id="scope" onClick={makeSope}></button>
+            )}
+        </div>
+    );
 }
